e5cd072626764ea2e2e1c8c0491f1be1895ee562
Igor Tkach <itkach@gmail.com>
2020-08-11 23:55:19 -0400

    Initial version with use-package

---
+++ init.el
@@ -0,0 +1,118 @@
+;; Do not show the startup screen.
+(setq inhibit-startup-message t)
+
+(when (window-system)
+  (set-frame-font "Fira Code"))
+
+
+;; Disable tool bar, menu bar, scroll bar.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Highlight current line.
+(global-hl-line-mode t)
+
+(global-set-key [(hyper v)] 'yank)
+(global-set-key [(hyper c)] 'kill-ring-save)
+
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
+
+;; Do not use `init.el` for `custom-*` code - use `custom-file.el`.
+(setq custom-file "~/.emacs.d/custom-file.el")
+
+;; Assuming that the code in custom-file is execute before the code
+;; ahead of this line is not a safe assumption. So load this file
+;; proactively.
+(load custom-file 'noerror)
+
+(setq auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
+(setq backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
+(column-number-mode t)
+(delete-selection-mode t)
+
+
+;; Require and initialize `package`.
+(require 'package)
+(package-initialize)
+
+;; Add `melpa` to `package-archives`.
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;; Additional packages and their configurations
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+  
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :ensure t
+  :config (doom-modeline-mode 1))
+
+(use-package iedit
+  :ensure t
+  :bind ("C-c ;" . iedit-mode))
+  
+(use-package company
+  :ensure t
+  ;; Navigate in completion minibuffer with `C-n` and `C-p`.
+  :bind (:map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  ;; Provide instant autocompletion.
+  (setq company-idle-delay 0.3)
+
+  ;; Use company mode everywhere.
+  (global-company-mode t))
+
+(use-package highlight-indent-guides
+  :ensure t
+  :config
+  (setq highlight-indent-guides-method 'character)
+  ;; Indent character samples: | ┆ ┊
+  (setq highlight-indent-guides-character ?\|)
+  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
+
+;; Display possible completions at all places
+(use-package ido-completing-read+
+  :ensure t
+  :config
+  ;; This enables ido in all contexts where it could be useful, not just
+  ;; for selecting buffer and file names
+  (ido-mode t)
+  (ido-everywhere t)
+  ;; This allows partial matches, e.g. "uzh" will match "Ustad Zakir Hussain"
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-filename-at-point nil)
+  ;; Includes buffer names of recently opened files, even if they're not open now.
+  (setq ido-use-virtual-buffers t)
+  :diminish nil)
+
+;; Git integration for Emacs
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit))
