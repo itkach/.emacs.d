5e2cb31ae1a3a9c973e4d320aa4deb7ec229489e
Igor Tkach <itkach@gmail.com>
2020-08-21 16:50:56 -0400

    Add haskell and nix mode

--- init.el
+++ init.el
@@ -281,6 +281,15 @@
   :config
   (add-hook 'prog-mode-hook 'highlight-symbol-mode))
 
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
+(use-package haskell-mode
+  :config
+  (setq haskell-font-lock-symbols t)
+  :hook ((haskell-mode . turn-on-haskell-doc-mode)
+         (haskell-mode . turn-on-haskell-indent)
+         (haskell-mode . interactive-haskell-mode)))
 
 (provide 'init)
 ;;; init.el ends here
